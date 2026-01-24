42feb4ddc5073b9681f6dd376d1c0be50034fcd0
23130384-TranCongVinh <23130384@st.hcmuaf.edu.vn>
2026-01-24 11:51:47 +0700

    Product Review

--- src/main/java/vn/edu/nlu/fit/projectweb/dao/ProductDao.java
+++ src/main/java/vn/edu/nlu/fit/projectweb/dao/ProductDao.java
@@ -29,6 +29,16 @@ public class ProductDao extends BaseDao {
 //        data.put(10,new Product(1,"Máy Ảnh Sony RX1R III | Chính Hãng","https://bizweb.dktcdn.net/100/107/650/products/8783339-sony-rx1riii-16.jpg?v=1752639489140",125500000));
 //
 //    }
+    public List<Product> getAll() {
+        String sql = "SELECT id, name FROM product";
+
+        return get().withHandle(handle ->
+                handle.createQuery(sql)
+                        .mapToBean(Product.class)
+                        .list()
+        );
+    }
+
     public List<Product> getListProduct() {
         return get().withHandle(h -> h.createQuery("select * from products").mapToBean(Product.class).list());
     }
